1ad02b5cb57d9666425bd19ac53faeb3bac24fa8
Karina <karinaklevzhits@gmail.com>
2022-04-19 20:44:27 +0300

    WSR-01/change module to class

--- lib/web_server_report/calculate_visits.rb
+++ lib/web_server_report/calculate_visits.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 # calculate number of visits
-module CalculateVisits
-  def number_of_visits_pages(data)
+class CalculateVisits
+  def initialize(data)
+    @data = data
+  end
+
+  def number_of_visits_pages
     hash_pages = Hash.new(0)
-    data.each do |page|
+    @data.each do |page|
       hash_pages[page[:page]] += 1
     end
-    p hash_pages.sort_by { |_key, value| -value }.uniq
+    hash_pages.sort_by { |_key, value| -value }.uniq
   end
 end
